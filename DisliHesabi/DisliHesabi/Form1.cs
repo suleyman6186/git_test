4beb1fba4db1f71cc88f1d9f531d6d12e41ce9a1
suleyman6186 <suleyman_6186@hotmail.com>
2014-07-19 12:46:00 +0300

    Dişli Hesabı Tasarıma Devam Edildi
    Programa , temizle butonu ve import butonu elendi.
    Doldurulması zorunlu alanlar boş bırakıldığında messagebox 'a uyarı
    mesajı gelmesi eklendi.
    Temizle butonu işlevini yapmakta, ancak import etme özelliği ise ileriki
    aşamalarda inşallah :)

--- DisliHesabi/DisliHesabi/Form1.cs
+++ DisliHesabi/DisliHesabi/Form1.cs
@@ -17,75 +17,125 @@ namespace DisliHesabi
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+
+        private void BtnHesapla1_Click(object sender, EventArgs e)
         {
-            
+            if (textBoxHelis1.Text == string.Empty)
+            {
+                MessageBox.Show("Mod Boş Bırakılamaz..!", "UYARI..!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBoxMod.Text == string.Empty)
+            {
+                MessageBox.Show("Pinyon Diş Sayısı Boş Bırakılmaz..!", "UYARI..!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBoxKavrama1.Text == string.Empty)
+            {
+                MessageBox.Show("Çark Diş Sayısı Boş Bırakılamaz..!", "UYARI..!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBoxPinyon1.Text == string.Empty)
+            {
+                MessageBox.Show("Kavrama Açısı Boş Bırakılamaz..!", "UYARI..!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBoxCark1.Text == string.Empty)
+            {
+                MessageBox.Show("Helis Açısı Boş Bırakılamaz..!", "UYARI..!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        private void label2_Click(object sender, EventArgs e)
+
+            //////////////////////////////////////////
+
+
+        private void BtnHesapla2_Click(object sender, EventArgs e)
         {
+            if (textBoxDiameter.Text == string.Empty)
+            {
+                MessageBox.Show("Diameter Pitch Boş Bırakılamaz..!", "UYARI..!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBoxPinyon2.Text == string.Empty)
+            {
+                MessageBox.Show("Pinyon Diş Sayısı Boş Bırakılmaz..!", "UYARI..!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBoxCark2.Text == string.Empty)
+            {
+                MessageBox.Show("Çark Diş Sayısı Boş Bırakılamaz..!", "UYARI..!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBoxKavrama2.Text == string.Empty)
+            {
+                MessageBox.Show("Kavrama Açısı Boş Bırakılamaz..!", "UYARI..!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBoxHelis2.Text == string.Empty)
+            {
+                MessageBox.Show("Helis Açısı Boş Bırakılamaz..!", "UYARI..!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }      
 
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tabPage2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
+        
+        private void BtnCikis1_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
-        private void tabPage1_Click(object sender, EventArgs e)
+        private void BtnCikis2_Click(object sender, EventArgs e)
         {
-
+            Application.Exit();
         }
 
-        private void textBox4_TextChanged(object sender, EventArgs e)
+        private void BtnTemizle1_Click(object sender, EventArgs e)
         {
+            textBoxMod.Clear();
+            textBoxPinyon1.Clear();
+            textBoxCark1.Clear();
+            textBoxKavrama1.Clear();
+            textBoxHelis1.Clear();
+            // Sonuçları Temizleme--Düz Mod
+            textBoxMod1.Clear();
+            textBoxMod2.Clear();
+            textBoxMod3.Clear();
+            textBoxMod4.Clear();
+            textBoxMod5.Clear();
+            textBoxMod6.Clear();
+            textBoxMod7.Clear();
+            textBoxMod8.Clear();
+            textBoxMod9.Clear();
+            textBoxMod10.Clear();
+            // Yardımcı Hesap Kısmı-Düz
+            textBoxKavrama3.Clear();
+            textBoxHelis3.Clear();
+            textBoxGercekKavrama.Clear();
+            textBoxDonmeDairesi.Clear();
+            textBoxEv1.Clear();
+            textBoxEv1.Clear();
+            textBoxHesap3.Clear();
+            textBoxY3.Clear();
+
 
         }
-
-        private void label39_Click(object sender, EventArgs e)
+        private void BtnTemizle2_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void label40_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox32_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label42_Click(object sender, EventArgs e)
-        {
-
-        }
-
+            textBoxDiameter.Clear();
+            textBoxPinyon2.Clear();
+            textBoxCark2.Clear();
+            textBoxKavrama2.Clear();
+            textBoxHelis2.Clear();
+            //Sonuçları Temizleme-- Dp
+            textBoxDp1.Clear();
+            textBoxDp2.Clear();
+            textBoxDp3.Clear();
+            textBoxDp4.Clear();
+            textBoxDp5.Clear();
+            textBoxDp6.Clear();
+            textBoxDp7.Clear();
+            textBoxDp8.Clear();
+            textBoxDp9.Clear();
+            // Yardımcı Hesap Kısmı-Dp
+            textBoxKavrama4.Clear();
+            textBoxHelis4.Clear();
+            textBoxGerçekKavrama4.Clear();
+            textBoxDönmeDairesi4.Clear();
+            textBoxEv4.Clear();
+            textBoxEv4.Clear();
+            textBoxHesap4.Clear();
+        }  
     }
 }
